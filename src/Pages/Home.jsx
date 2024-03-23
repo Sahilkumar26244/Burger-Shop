9263ec8c4eeb4d546b73ef7710a0c8362ba9abc1
Sahil Kumar <kumarsahil8981@gmail.com>
2024-03-23 14:58:13 +0530

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
+
+function Home() {
+  return (
+    <div>
+    <Navbar/>
+        Home
+    <Footer/>
+    </div>
+  )
+}
+
+export default Home
